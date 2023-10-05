19b9a3b97f2d4fddcc4ec82cd69a6085cba31fb5
ishaancodes011 <ishaancodes011@gmail.com>
2023-10-05 14:27:15 +0530

    Check 1

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.title("Hello World")
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+st.title("Hello World!")
